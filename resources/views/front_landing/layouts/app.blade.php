43f8bcc3ade65418424f5606338d58b58660a215
eyadmanna <eyadmanna@gmail.com>
2025-07-12 15:05:29 +0300

    add userway widget

--- resources/views/front_landing/layouts/app.blade.php
+++ resources/views/front_landing/layouts/app.blade.php
@@ -137,7 +137,10 @@
     </script>
     <script src="{{ mix('js/front-pages.js') }}"></script>
     <script src="https://js.stripe.com/v3/"></script>
-    @yield('page_js')
+    <script src="https://cdn.userway.org/widget.js" data-account="ZUGBkHHNS8"></script>
+    
+        @yield('page_js')
+
 </head>
 
 <body class="body-wrapper">
@@ -163,6 +166,7 @@
 <a href="{{$settings['phone'] }}" class="floating-contact-button" target="_blank">
     <img src="{{ asset('front_landing/images/WhatsApp.svg') }}" alt="WhatsApp" />
 </a>
+
 
 <button id="backToTopBtn" class="back-to-top" style="background-color: #00c5ff">
     ▲
